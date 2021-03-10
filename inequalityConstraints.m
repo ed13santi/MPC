add54595f9179940747f044545f9d2b94f8e64a5
ed13santi <37627980+ed13santi@users.noreply.github.com>
2021-03-10 20:08:59 +0000

    final commit wedensday

--- inequalityConstraints.m
+++ inequalityConstraints.m
@@ -13,10 +13,10 @@ function [A, b] = inequalityConstraints(N, r, tolerances, ropeLen, rectConstrain
         [A3, b3] = ellipseLimsRows(ellipses, w(10*i+1), w(10*i+3));
         
         
-        A_tmp = [A1;A2;A3];
+        A_tmp = [A1;A2];
         A_tmp2 = [zeros(size(A_tmp,1), i*10), A_tmp, zeros(size(A_tmp,1), 8+10*N-i*10-10)];
         A = [A; A_tmp2];
-        b = [b; b1;b2;b3];
+        b = [b; b1;b2];
         
 %         if N - i < n_final_pos_constrs
 %             % final position constraint
