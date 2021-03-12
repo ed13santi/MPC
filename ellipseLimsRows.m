4f05749aab914cf5ffb8c4ac07b39660abf662cc
ed13santi <37627980+ed13santi@users.noreply.github.com>
2021-03-12 19:15:03 +0000

    only use 2 most importnat ellipses at each time step

--- ellipseLimsRows.m
+++ ellipseLimsRows.m
@@ -1,14 +1,36 @@
 function [ARows, bRows] = ellipseLimsRows(ropeLen, ellipses, xg, yg, thetag, phig)
-    ARows = zeros(0,10);
-    bRows = zeros(0,1);
+    ellList = [];
+    ellVals = [];
+    ellObjVals = [];
     for j=1:size(ellipses,1)
         for z=1:size(ellipses,2)
             ell = ellipses{j,z};
-            [ARow1, bRow1] = lineariseEllipse(xg, yg, ell.xc, ell.yc, ell.a, ell.b);
-            [ARow2, bRow2] = lineariseEllipseObject(ropeLen, xg, yg, thetag, phig, ell.xc, ell.yc, ell.a, ell.b);
-            ARows = [ARows; ARow1; ARow2];
-            bRows = [bRows; bRow1; bRow2];
+            ellList = [ ellList; ell ];
+            % build list of the values of ellipseEval for cart
+            ellVals = [ ellVals; ellipseEval(xg, yg, ell.xc, ell.yc, ell.a, ell.b) ];
+            % build list of the values of ellipseEval for object
+            ellObjVals = [ ellObjVals; ellipseEval(xg+ropeLen*sin(thetag), yg+ropeLen*sin(phig), ell.xc, ell.yc, ell.a, ell.b) ];
         end
     end
+    [~,indexesCart] = sort(ellVals, 'ascend'); % indexes in order of most active due to cart
+    [~,indexesObj] = sort(ellObjVals, 'ascend'); % indexes in order of most active due to object
+    
+    ARows = zeros(length(ellList)*2,10);
+    bRows = zeros(length(ellList)*2,1);
+    
+    % constraints of cart
+    for ellInd=1:indexesCart(1:2)
+        ell = ellList(ellInd);
+        [ARow1, bRow1] = lineariseEllipse(xg, yg, ell.xc, ell.yc, ell.a, ell.b);
+        ARows = [ARows; ARow1];
+        bRows = [bRows; bRow1];
+    end 
+    %constraints of object
+    for ellInd=1:indexesObj(1:2)
+        ell = ellList(ellInd);
+        [ARow2, bRow2] = lineariseEllipseObject(ropeLen, xg, yg, thetag, phig, ell.xc, ell.yc, ell.a, ell.b);
+        ARows = [ARows; ARow2];
+        bRows = [bRows; bRow2];
+    end 
 end
 
